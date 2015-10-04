5c1a0ed851221c9a4d6863552b82ebcfb0fd25df
Yuki Sako <yukisakoatoz@gmail.com>
2015-10-04 23:50:16 +0900

    add recommend list

--- app/controllers/recommend_list_controller.rb
+++ app/controllers/recommend_list_controller.rb
@@ -1,4 +1,11 @@
 class RecommendListController < ApplicationController
   def recommend
+    if params[:q]
+      response = Amazon::Ecs.item_search(params[:q]+" お土産" , 
+                                  :search_index => 'All' , 
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
+      @amazon_items = response.items
+    end
   end
 end
